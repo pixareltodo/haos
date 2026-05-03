032cd56861e6145221eaeb762aeff9b4135a6517
pixareltodo <pixar@eltodo.cz>
2026-05-03 10:16:47 +0200

    Add manifest builder

---
+++ stremio_csfd_catalogs/src/stremio/manifest.js
@@ -0,0 +1,16 @@
+export function buildManifest(options, catalogs) {
+  const types = Array.from(new Set(catalogs.map((catalog) => catalog.type)));
+
+  return {
+    id: options.addon_id,
+    version: options.addon_version || '1.0.5',
+    name: options.addon_name,
+    description: 'Local Stremio addon for CSFD movie catalogs',
+    resources: [
+      { name: 'catalog', types },
+      { name: 'meta', types, idPrefixes: ['csfd:'] }
+    ],
+    types,
+    catalogs
+  };
+}
