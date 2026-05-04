ea549091a946a7241b3d2cbbcf8fed01a59ec0a6
pixareltodo <pixar@eltodo.cz>
2026-05-04 17:11:07 +0200

    Bump addon version to 1.0.21

--- stremio_csfd_catalogs/src/config/buildInfo.js
+++ stremio_csfd_catalogs/src/config/buildInfo.js
@@ -1,4 +1,4 @@
 export const BUILD_INFO = {
-  version: '1.0.20',
-  buildSignature: '1.0.20-trakt-list-id-and-app-copy-2026-05-04'
+  version: '1.0.21',
+  buildSignature: '1.0.21-trakt-batch-retry-and-matched-default-2026-05-04'
 };
