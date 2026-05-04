7c23b3da3456c08bf38b0e7518a6f0425cdb6227
pixareltodo <pixar@eltodo.cz>
2026-05-04 13:42:44 +0200

    Bump build info to 1.0.18

--- stremio_csfd_catalogs/src/config/buildInfo.js
+++ stremio_csfd_catalogs/src/config/buildInfo.js
@@ -1,4 +1,4 @@
 export const BUILD_INFO = {
-  version: '1.0.17',
-  buildSignature: '1.0.17-trakt-export-create-without-lists-2026-05-04'
+  version: '1.0.18',
+  buildSignature: '1.0.18-trakt-limit-ui-and-manual-list-2026-05-04'
 };
